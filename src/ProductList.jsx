06586c28d23d2c536296975b386eb5492a66ad3b
Akhilesh Sarasan <akhileshsarasan@gmail.com>
2026-08-15 14:12:19 -0400

    Updated CartSlice.jsx with reducer functions

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { addItem, removeItem, updateQuantity } from './CartSlice';
+import { addItem } from './CartSlice';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
